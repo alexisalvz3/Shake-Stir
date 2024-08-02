9276dff5ad78a3b84a19c94ebdbac47b474658bb
alexisalvz3 <alexis.alvz3@gmail.com>
2024-08-01 22:38:18 -0700

    adjusted axios fetch function and .env files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/api/hello`)
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
     </div>
   );
 }
